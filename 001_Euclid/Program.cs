c83cc9b29c28a27c0ac3eae9732e5f81d8587075
KIMS0K0 <skkim120121@gmail.com>
2023-10-09 02:59:10 +0900

    초기 커밋.

---
+++ 001_Euclid/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_Euclid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
